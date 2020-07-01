e74211d9361c3c36324810009b0b103545aee4a9
michael20996 <michael20996@gmail.com>
2020-07-01 11:17:31 -0700

    created react app

---
+++ build/precache-manifest.18e390ec0d0f67eb7f16c611d4d467a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c1011c3ab65f313a8b93d76a128d9b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fa02f5f327bbd2b2106",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "caef03def8ce13397fca",
+    "url": "/static/js/2.0fc95c8a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0fc95c8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fa02f5f327bbd2b2106",
+    "url": "/static/js/main.baa39fdc.chunk.js"
+  },
+  {
+    "revision": "c74b6dcb725f329d7bfe",
+    "url": "/static/js/runtime-main.403e0656.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css';
+import SearchResultContainer from './components/SearchResultContainer';
+
+function App() {
+  return <SearchResultContainer/>
+}
+
+export default App;
